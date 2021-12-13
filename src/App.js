3937125896afa90b930f3270652077ee017a9e61
Jiyong Sim <sjy19910222@gmail.com>
2021-12-14 00:50:52 +0900

    #Cleanup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import styles from "./App.module.css";
 import Button from "./Button";
 import {useState, useEffect} from "react";
 
+function Hello () {
+  useEffect(() => {
+    console.log("created :)");
+    
+    // 'Cleanup Function'이라고 하고, 해당 component가 destroy될 때 뭔가 할 수 있도록 해줌
+    return () => console.log("destroyed :(")
+  
+  }, []);
+  return <h1>Hello</h1>
+};
+
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
 
-  const onClick = () => setValue((prev)=>prev + 1);
+  const onClickCounter = () => setValue((prev)=>prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+  const onClickShowing = () => setShowing((prev) => !prev);
 
-  // useEffect는 한 번만 호출 시킨다.
+  // useEffect는 코드를 언제 실행할지 선택하는것이다.
+  // dependency가 비어 있으므로 한번만 호출, 다음 부터는 호출 하지 않음
   useEffect(() => {
     console.log("I run only once.")
   }, []);
@@ -19,6 +33,7 @@ function App() {
     console.log("i run when 'keyword' changes")
   }, [keyword]);
 
+  // 'counter'가 변화할 때 코드를 실행할 거라고 react.js에게 알려주는 것
   useEffect(() => {
     console.log("i run when 'counter' changes")
   },[counter]);
@@ -36,7 +51,11 @@ function App() {
       <div>
         <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
         <h1 className={styles.title}>{counter}</h1>
-        <button onClick={onClick}>Click me!!</button>
+        <button onClick={onClickCounter}>Click me!!</button>
+      </div>
+      <div>
+        {showing ? <Hello /> : null}
+        <button onClick={onClickShowing}>{showing ? "Hide" : "Showing"}</button>
       </div>
     </div>
   );
