e1f82302b2f529155e3bfe46dd490c9b841b7864
Jiyong Sim <sjy19910222@gmail.com>
2021-12-14 00:22:31 +0900

    #Deps

--- src/App.js
+++ src/App.js
@@ -4,17 +4,40 @@ import {useState, useEffect} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setValue((prev)=>prev + 1);
-  console.log("I'm run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
+
+  // useEffect는 한 번만 호출 시킨다.
   useEffect(() => {
-    console.log("CALL THE API....")
+    console.log("I run only once.")
   }, []);
+
+  // 'keyword'가 변화할 때 코드를 실행할 거라고 react.js에게 알려주는 것
+  useEffect(() => {
+    console.log("i run when 'keyword' changes")
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("i run when 'counter' changes")
+  },[counter]);
+
+  useEffect(() => {
+    console.log("i run when keyword & counter changes")
+  },[keyword, counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!</h1>
-      <Button text={"Continue"}/>
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click me!!</button>
+      <div>
+        <h1 className={styles.title}>Welcome Back!!</h1>
+        <Button text={"Continue"}/>
+      </div>
+      <div>
+        <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
+        <h1 className={styles.title}>{counter}</h1>
+        <button onClick={onClick}>Click me!!</button>
+      </div>
     </div>
   );
 }
