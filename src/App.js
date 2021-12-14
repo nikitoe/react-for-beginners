5c0693f98a564d255102698e6218d7118e6efd50
Jiyong Sim <sjy19910222@gmail.com>
2021-12-14 15:26:23 +0900

    #7.1 To Do List part Two

--- src/App.js
+++ src/App.js
@@ -21,6 +21,13 @@ function App() {
         <input onChange={onChange} value = {toDo} type = "text" placeholder="Write your to do..."/>
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+        ))}
+      </ul>
+     
     </div>
   );
 };
