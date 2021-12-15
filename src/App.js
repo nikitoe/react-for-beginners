b6d4c0939913f4d360c36e3140cd4aca0405417d
Jiyong Sim <sjy19910222@gmail.com>
2021-12-15 23:49:58 +0900

    #7.4 Movie App part Two

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
 import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
-function MovieApp() {
-  const [movieLoading, setMovieLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-        )
-    ).json();
-    setMovies(json.data.movies);
-    setMovieLoading(false);
-  };
-  
-  useEffect(() => {
-    getMovies();
-  },[]);
-
-  console.log(movies);
-
-  return (
-    <div>
-      {movieLoading ? (<h1>Loading...</h1>) : (
-      <div>
-        {movies.map((movie) => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres.map((g) => (<li key = {g}>{g}</li>))}
-            </ul>
-          </div>
-        ))}
-      </div>)};
-    </div>
-  );
+function App() {
+  return null;
 };
 
 function CoinTracker () {
@@ -63,7 +30,7 @@ function CoinTracker () {
   );
 };
 
-function App() {
+function Example() {
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
 
@@ -81,7 +48,11 @@ function App() {
     <div>
       <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
-        <input onChange={onChange} value = {toDo} type = "text" placeholder="Write your to do..."/>
+        <input
+            onChange={onChange}
+            value={toDo}
+            type="text"
+            placeholder="Write your to do..."/>        
         <button>Add To Do</button>
       </form>
       <hr />
@@ -91,7 +62,7 @@ function App() {
         ))}
       </ul>
       <hr /><CoinTracker />
-      <hr /><MovieApp />
+      
     </div>
   );
 };
